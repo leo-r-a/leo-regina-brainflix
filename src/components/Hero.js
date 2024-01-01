b3f1656f818d2289bea74c13228638dcf5929ec8
Leo Regina <spleed.net@gmail.com>
2023-12-31 20:11:46 -0500

    adding video data to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,41 @@
+import videos from "../Data/videos.json";
+import "./Hero.scss";
+import eyeball from "../assets/Icons/views.svg"
+import heart from "../assets/Icons/likes.svg"
+import videodetails from "../Data/video-details.json"
 
-import "./Hero.scss"
+export default function Hero({selectedVideo}) {
+ 
 
+    const getSelectedVideoDetails = videodetails.find(
+       video => video.id === selectedVideo.id 
+    )
 
-export default function Hero() {
-    return (
-        <div className='hero'>
-            <video className="hero__video" controls>
-                <source src={video} type="video/mp4"/>
-            </video>
+    const date = new Date(getSelectedVideoDetails.timestamp);
+    const month = date.getMonth() +1;
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const parsedDate = month + '/' + day + '/' + year;
+
+  return (
+    <div className="hero">
+      <video className="hero__video" controls  poster={getSelectedVideoDetails.image}>
+        <source src={getSelectedVideoDetails.image} type="video/mp4" />
+      </video>
+      <div className="main__header">
+        <h1>{getSelectedVideoDetails.title}</h1>
+        <div className="main__info">
+          <h2 className="main__info--channel">{getSelectedVideoDetails.channel}</h2>
+          <img className="main__info--views-image" src={eyeball} />
+          <p className="main__info--views">{getSelectedVideoDetails.views}</p>
+          <p className="main__info--date">{parsedDate}</p>
+          <img className="main__info--likes-image" src={heart} />
+          <p className="main__info--likes">{getSelectedVideoDetails.likes}</p>
         </div>
-    );
+        <div className="main__info--video-description">
+          {getSelectedVideoDetails.description}
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Nextvids.js
@@ -0,0 +1,9 @@
+function Nextvids() {
+    return (
+        <div className="video-container">
+            <p className="video-header" >NEXT VIDEOS</p>
+            <div className="video-list"></div>
+            
+        </div>
+    )
+}
